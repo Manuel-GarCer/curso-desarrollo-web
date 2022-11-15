1451b5f6f1d1a571ec7090878dfb10b444f47641
Manuel-GarCer <manoloache@gmail.com>
2022-11-14 22:34:22 -0500

    clase de JS 14/11/22

--- 02 JAVASCRIPT/CAPITULO 01/js/code-02.js
+++ 02 JAVASCRIPT/CAPITULO 01/js/code-02.js
@@ -9,3 +9,16 @@ let radio = 11.5;
 //como se consigue el area de un circulo ??
 let areaCirculo = pi * radio ** 2;
 console.log(areaCirculo);
+
+// residuo
+let num3 = 12;
+console.log(num3 % 2);
+if(num3 % 2 == 0){
+    console.log("que el numero es par");
+} else {
+    console.log("el numero es impar")
+}
+
+//suma y resta
+let num4 = "10";
+let num5 = 5.5;
---
+++ 02 JAVASCRIPT/CAPITULO 01/js/code-03.js
@@ -0,0 +1,13 @@
+const nombre = "Jaimito";
+const apellido = "Rosales";
+const fullName = nombre + " " + apellido;
+console.log(fullName);
+
+let edad = 999;
+
+const fullDataTs = `hola soy ${fullName}` +  ` , y tengo ` + edad + " años de edad";
+console.log(fullDataTs);
+
+const suma = `${edad + 100}`;
+console.log(suma);
+
